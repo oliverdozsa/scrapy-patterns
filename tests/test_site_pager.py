e842c2d5190a9f19fe869a0375cfb2ab20ae3246
Oliver Dozsa <dozsa.oliver@gmail.com>
2019-12-16 22:24:38 +0100

    Added a new test.

--- tests/test_site_pager.py
+++ tests/test_site_pager.py
@@ -30,24 +30,51 @@ def test_process_page_has_next():
     pager.start("http://some-starting-url.com")
 
     # Starting URL has 1 item, and has next page.
-    next_item_reqs_generator = mock_request_factory.create.call_args[0][1]
-    mock_response = Mock()
-    parser.next_page_url.has_next.return_value = True
-    parser.next_page_url.parse.return_value = "http://some-next-item-url.com"
-    parser.item_urls.parse.return_value = ["http://item1.url"]
+    __simulate_page_response(mock_request_factory, parser, True, "http://some-next-page-url.com")
 
     # Item URL check
-    _ = [req for req in next_item_reqs_generator(mock_response)]
     mock_request_factory.create.assert_called_with("http://item1.url", ANY, errback=ANY)
 
-    # Since no more itesm, check next page URL
-    next_item_reqs_generator = mock_request_factory.create.call_args[0][1]
-    mock_item_response = Mock()
-    _ = [req for req in next_item_reqs_generator(mock_item_response)]
-    mock_request_factory.create.assert_called_with("http://some-next-item-url.com", ANY)
+    # Since no more items, check next page URL
+    __simulate_items_response(mock_request_factory)
+    mock_request_factory.create.assert_called_with("http://some-next-page-url.com", ANY)
+
+
+def test_process_has_no_next():
+    """Tests the last page reached scenario."""
+
+    # Starting URL
+    mock_request_factory = Mock()
+    parser = __create_mock_site_page_parser()
+    pager = SitePager(Mock(), mock_request_factory, parser)
+    pager.start("http://some-starting-url.com")
+
+    # Starting URL has 1 item, and has next page.
+    __simulate_page_response(mock_request_factory, parser, False)
+
+    # Since no more items, next items, or requests
+    next_items_or_reqs = __simulate_items_response(mock_request_factory)
+    assert len(next_items_or_reqs) == 2, "There should be items after item response!"
+    assert next_items_or_reqs[1] is None, "The request after item should be None!"
 
 
 def __create_mock_site_page_parser():
     parser = SitePageParsers(Mock(), Mock(), Mock())
     return parser
 
+
+def __simulate_page_response(mock_req_factory: Mock, mock_site_parser: SitePageParsers,
+                             has_next_page: bool, next_page_url: str = None):
+    # With one item on page
+    next_item_urls_reqs_generator = mock_req_factory.create.call_args[0][1]
+    mock_response = Mock()
+    mock_site_parser.next_page_url.has_next.return_value = has_next_page
+    mock_site_parser.next_page_url.parse.return_value = next_page_url
+    mock_site_parser.item_urls.parse.return_value = ["http://item1.url"]
+    return list(next_item_urls_reqs_generator(mock_response))  # Next item URL request
+
+
+def __simulate_items_response(mock_req_factory: Mock):
+    next_items_or_reqs_generator = mock_req_factory.create.call_args[0][1]
+    mock_item_response = Mock()
+    return list(next_items_or_reqs_generator(mock_item_response))  # Next item, and next page
