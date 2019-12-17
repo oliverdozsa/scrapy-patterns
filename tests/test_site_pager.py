b228dd8b99cc4bee776265f3130518f7469b665d
Oliver Dozsa <dozsa.oliver@gmail.com>
2019-12-17 12:22:50 +0100

    Added remaining tests for site pager.

--- tests/test_site_pager.py
+++ tests/test_site_pager.py
@@ -1,6 +1,8 @@
 """Contains site pager tests"""
-from unittest.mock import Mock, ANY
+from unittest.mock import Mock, ANY, call
+import pytest
 from scrapy_patterns.spiderlings.site_pager import SitePager, SitePageParsers
+from scrapy.exceptions import DontCloseSpider
 
 
 def test_create():
@@ -29,18 +31,21 @@ def test_process_page_has_next():
     pager = SitePager(Mock(), mock_request_factory, parser)
     pager.start("http://some-starting-url.com")
 
-    # Starting URL has 1 item, and has next page.
-    __simulate_page_response(mock_request_factory, parser, True, "http://some-next-page-url.com")
+    __simulate_page_response_with_items(mock_request_factory, parser, True, 2, "http://some-next-page-url.com")
 
-    # Item URL check
-    mock_request_factory.create.assert_called_with("http://item1.url", ANY, errback=ANY)
+    # Expect request factory calls for starting URls, and two items.
+    calls = [call.create("http://some-starting-url.com", ANY),
+             call.create("http://item1.url", ANY, errback=ANY),
+             call.create("http://item2.url", ANY, errback=ANY)]
+    mock_request_factory.assert_has_calls(calls)
 
-    # Since no more items, check next page URL
+    # Receive two items, and expect going to the next page.
+    __simulate_items_response(mock_request_factory)
     __simulate_items_response(mock_request_factory)
     mock_request_factory.create.assert_called_with("http://some-next-page-url.com", ANY)
 
 
-def test_process_has_no_next():
+def test_process_page_has_no_next():
     """Tests the last page reached scenario."""
 
     # Starting URL
@@ -49,13 +54,60 @@ def test_process_has_no_next():
     pager = SitePager(Mock(), mock_request_factory, parser)
     pager.start("http://some-starting-url.com")
 
-    # Starting URL has 1 item, and has next page.
-    __simulate_page_response(mock_request_factory, parser, False)
+    __simulate_page_response_with_items(mock_request_factory, parser, False, 2)
 
     # Since no more items, next items, or requests
-    next_items_or_reqs = __simulate_items_response(mock_request_factory)
-    assert len(next_items_or_reqs) == 2, "There should be items after item response!"
-    assert next_items_or_reqs[1] is None, "The request after item should be None!"
+    next_items_or_reqs_for_first_response = __simulate_items_response(mock_request_factory)
+    next_items_or_reqs_for_second_response = __simulate_items_response(mock_request_factory)
+    for next_items_reqs in [next_items_or_reqs_for_first_response, next_items_or_reqs_for_second_response]:
+        assert len(next_items_reqs) == 2, "There should be items after item response!"
+        assert next_items_reqs[1] is None, "The request after item should be None!"
+
+
+def test_request_item_failures_with_next_page():
+    """Tests item request failure."""
+
+    mock_request_factory = Mock()
+    parser = __create_mock_site_page_parser()
+    mock_spider = Mock()
+    pager = SitePager(mock_spider, mock_request_factory, parser)
+    pager.start("http://some-starting-url.com")
+
+    __simulate_page_response_with_items(mock_request_factory, parser, True, 1, "http://some-next-page-url.com")
+
+    failure_callback = mock_request_factory.create.call_args[1]["errback"]
+    failure_callback(Mock())
+    # Since the last item failed,spider_idle signal will be triggered.
+    spider_idle_callback = mock_spider.crawler.signals.connect.call_args[0][0]
+    with pytest.raises(DontCloseSpider):
+        spider_idle_callback(mock_spider)
+        mock_spider.crawler.engine.crawl.assert_called()
+
+
+def test_page_and_items_with_request_kwargs():
+    """Test when the item, and page urls also have request keywords attached to them"""
+    # Starting URL
+    mock_request_factory = Mock()
+    parser = __create_mock_site_page_parser()
+    pager = SitePager(Mock(), mock_request_factory, parser)
+    pager.start("http://some-starting-url.com")
+
+    process_page_callback = mock_request_factory.create.call_args[0][1]
+    mock_response = Mock()
+    parser.next_page_url.has_next.return_value = True
+    parser.next_page_url.parse.return_value = ("http://some-next-page-url.com", {"some_page_req_kwarg": "some_value"})
+    parser.item_urls.parse.return_value = [("http://item1.url", {"some_item_req_kwarg": "some_other_value"})]
+    _ = list(process_page_callback(mock_response))
+
+    req_kwargs = mock_request_factory.create.call_args[1]
+    assert "some_item_req_kwarg" in req_kwargs
+    assert req_kwargs["some_item_req_kwarg"] == "some_other_value"
+
+    process_item_callback = mock_request_factory.create.call_args[0][1]
+    _ = list(process_item_callback(Mock()))
+    next_page_req_kwarg = mock_request_factory.create.call_args[1]
+    assert "some_page_req_kwarg" in next_page_req_kwarg
+    assert next_page_req_kwarg["some_page_req_kwarg"] == "some_value"
 
 
 def __create_mock_site_page_parser():
@@ -63,18 +115,17 @@ def __create_mock_site_page_parser():
     return parser
 
 
-def __simulate_page_response(mock_req_factory: Mock, mock_site_parser: SitePageParsers,
-                             has_next_page: bool, next_page_url: str = None):
-    # With one item on page
-    next_item_urls_reqs_generator = mock_req_factory.create.call_args[0][1]
+def __simulate_page_response_with_items(mock_req_factory: Mock, mock_site_parser: SitePageParsers,
+                                        has_next_page: bool, num_of_items=1, next_page_url: str = None):
+    process_page_callback = mock_req_factory.create.call_args[0][1]
     mock_response = Mock()
     mock_site_parser.next_page_url.has_next.return_value = has_next_page
     mock_site_parser.next_page_url.parse.return_value = next_page_url
-    mock_site_parser.item_urls.parse.return_value = ["http://item1.url"]
-    return list(next_item_urls_reqs_generator(mock_response))  # Next item URL request
+    mock_site_parser.item_urls.parse.return_value = ["http://item%d.url" % (i + 1) for i in range(0, num_of_items)]
+    return list(process_page_callback(mock_response))  # Next item URL request
 
 
 def __simulate_items_response(mock_req_factory: Mock):
-    next_items_or_reqs_generator = mock_req_factory.create.call_args[0][1]
+    process_item_callback = mock_req_factory.create.call_args[0][1]
     mock_item_response = Mock()
-    return list(next_items_or_reqs_generator(mock_item_response))  # Next item, and next page
+    return list(process_item_callback(mock_item_response))  # Next item, and next page
